e70a76939afaedff730c7a8aa9195b7ad7fae8e0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-08-04 09:08:46 +0100

    learning sweetjs https://www.sweetjs.org/doc/tutorial'

--- operator-min.js
+++ operator-min.js
@@ -1,8 +1,8 @@
 
-operator @ right 10 = (left, right) => {
+operator @ left 20 = (left, right) => {
   return #`Math.min(${left}, ${right})`;
 };
 
 
-console.log( 3 @ 2); // 2
-console.log( 3 @ 4); // 3
+console.log( 3 @ 2 - 1);  // 1
+console.log( 3 @ 4 <= 3); // true
